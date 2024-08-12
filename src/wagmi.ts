1db092f636d1176c865c3e35d22e99497a260938
davit-khachatryan1 <khachatryandavit55@gmail.com>
2024-08-12 13:12:09 +0400

    .

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,6 +1,8 @@
 import { http, cookieStorage, createConfig, createStorage } from "wagmi";
 import { mainnet, sepolia, base } from "wagmi/chains";
-import { coinbaseWallet, injected, metaMask, walletConnect } from "wagmi/connectors";
+import {
+  metaMask,
+} from "wagmi/connectors";
 import { polygonAmoy } from "./chains"; // Import the custom chain
 
 const projectId = "9211cd59958f85a6b25688482536e012";
@@ -11,8 +13,8 @@ export function getConfig() {
     connectors: [
       metaMask({
         dappMetadata: {
-          name: "Nft transaction app", 
-          url: "https://nft-transaction-app.vercel.app/", 
+          name: "Nft transaction app",
+          url: "https://nft-transaction-app.vercel.app/",
         },
       }),
     ],
